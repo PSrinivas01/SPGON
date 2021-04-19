466005e4c74ec45d5f3ac60018d6a013220d27e0
PSrinivas01 <82811388+PSrinivas01@users.noreply.github.com>
2021-04-19 19:35:59 +0530

    Delete EAMPLE 1 ANSWER.py

--- EAMPLE 1 ANSWER.py
+++
@@ -1,7 +0,0 @@
-input_string=input()[::-1]
-output_list=[]
-for each_char in input_string:
-    a=ord(each_char)
-    output_list.append(chr(a+1))
-output_string="".join(output_list)
-print(output_string)
